ad7bb734bcf6b39e7317f6b06c28353b49b90591
Mohan Puttamadaiah <mohan.p3@gmail.com>
2022-06-07 18:17:35 +0100

    Youtube link.

--- src/main/java/com/javatechie/executor/api/config/CustomAsyncExceptionHandler.java
+++ src/main/java/com/javatechie/executor/api/config/CustomAsyncExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 @Slf4j
 public class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
@@ -13,9 +14,8 @@ public class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandle
     public void handleUncaughtException(final Throwable throwable, final Method method, final Object... obj) {
        log.info("Exception message - " + throwable.getMessage());
         log.info("Method name - " + method.getName());
-        for (final Object param : obj) {
-            log.info("Param - " + param);
-        }
+
+        Arrays.stream(obj).forEach(param -> log.info("Param - " + param));
     }
 
 }
